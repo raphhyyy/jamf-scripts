37c11aedff47ca1b9a6524008ed353fb376c4ae4
Raphael Hernandez <rhernandez1@2u.com>
2024-03-27 09:08:53 -0500

    modifed Computer name text output as all devices are defaulted as Macbook Pro upon imaging. This will prevent confusion in our Slack channel

--- jamfreporter.sh
+++ jamfreporter.sh
@@ -1,38 +1,27 @@
 #!/bin/bash
+WEBHOOK_URL=""
+IMAGE=""
 
-# Last Modified by Raphael Hernandez 03/7/2024
-
-WEBHOOK_URL=<SLACK WEBHOOK URL>
-IMAGE=<EXTERNALLY HOSTED IMAGE>
-
-JSS=<JSS ADDRESS>
+JSS=""
 # API Account that has read access to Jamf Pro Server Objects > Computers
-API_USER=<API USERNAME>
-API_PW=<API PASSWORD>
+API_USER=""
+API_PW=""
 
 UDID=$(system_profiler SPHardwareDataType | grep "UUID" | sed s/"Hardware UUID: "/''/g | sed 's/^ *//')
-SITE_NAME=$(/usr/bin/curl -H "Accept: application/xml" -H "Content-type: application/xml" -s -u "$API_USER":"$API_PW" "${JSS}/JSSResource/computers/udid/$UDID/subset/General" | xpath /computer/general/site/name | sed -e 's/<name>//;s/<\/name>//')
-COMPUTER_NAME=$(scutil --get ComputerName)
 
-SSID=<WIFI SSID>
-MODEL=$(system_profiler SPHardwareDataType | grep "Model Name" | sed 's/^[ \t]*//' | sed 's/Model Name: //g')
+SSID=""
+SERIAL=$(system_profiler SPHardwareDataType | grep "Serial" | sed 's/^[ \t]*//' | sed 's/Serial: //g')
 OS_VERSION=$(/usr/bin/sw_vers -productVersion)
 
 # Danger status set to false
 danger=0
 
 TITLE="Mac Setup Completed"
-TEXT="*JSS Site:* $SITE_NAME\n*Computer name:* $COMPUTER_NAME\n*Mac model:* $MODEL\n*macOS version:* $OS_VERSION\n"
+TEXT="*Mac serial:* $SERIAL\n*macOS version:* $OS_VERSION\n"
  
 # Primary User Check - looks at computer's assigned 'endUsername' and then confirms that they are an fv enabled user
 PRIMARY_USER=$(/usr/bin/curl -H "Accept: application/xml" -H "Content-type: application/xml" -s -u "$API_USER":"$API_PW" "${JSS}/JSSResource/computers/udid/$UDID"/subset/Location | xpath /computer/location/username | sed -e 's/<username>//;s/<\/username>//')
 
-if [[ -z "$PRIMARY_USER" ]]
-then
-	PRIMARY_USE="Unassigned"
-fi
-
-TEXT="$TEXT*Primary User:* $PRIMARY_USER\n"
 
 if [[ "$PRIMARY_USER" != "Unassigned" ]]
 then
@@ -54,10 +43,10 @@ wifi_check=$(echo "$WIFI" | grep "$SSID")
     
 if [[ -z "$wifi_check" ]]
 then 
-	TEXT="$TEXT*WiFi Check*: Mac not connected to $SSID\n"
+	TEXT="$TEXT*WiFi Check*: Not connected $SSID\n"
 	danger=1
 else
-	TEXT="$TEXT*WiFi Check*: Connected to $SSID\n"
+	TEXT="$TEXT*WiFi Check*: Connected $SSID\n"
 fi
   
 # If anything was deemed to have failed in the checks above, then the post color will be set to 'danger'/red
